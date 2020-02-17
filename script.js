d3001b2a934614d343b100166875868ac492e78c
sonihidayatulloh797@gmail.com <sonihidayatulloh797@gmail.com>
2020-02-18 05:40:05 +0700

    update pop up box

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-alert("Hello world my name is soni");
+// Part 1 = alert 
+// =============================================================
+// alert("Hallo");
+// alert("nama saya");
+// alert("Soni Hidayatulloh");
+
+// prompt("Silahkan masukan nama anda : ")
+// prompt("Masukan alamat anda : ")
+
+// Reapet for 
+// ==============================================================
+// for(i=0; i < 5; i++){
+//     alert("Hello my name is Soni");
+// }
+
+// =====================================================================
 
 
+
+// Part 2 = prompt
+
+// var nama   = prompt("Masukan nama anda :"),
+//     alamat = prompt("Masukan alamat anda :"),
+//     hobi   = prompt("Masukan hobi anda :");
+
+// alert("Hello" + " " + nama);
+// alert("Alamat anda di" + " " + alamat);
+// alert(hobi);
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 // var person = prompt("Please enter your name", "Soni hidayatulloh");
 
 // if( person == null || person == ""){
@@ -8,3 +35,34 @@ alert("Hello world my name is soni");
 // } else {
 //     txt = "Hello" + person + "How are you today ?";
 // }
+
+
+// =======================================================================
+// //Part 2 Confirm
+
+// var tes = confirm("Apakah kamu yakin ??");
+// if( tes === true ){
+//     alert("user Menekan tombol oke!")
+// } else {
+//     alert("User menekan CANCEL !!") 
+// }
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// confirm Repeation while
+// =======================================================================
+// alert("Selamat datang ..");
+// var lagi = true
+
+// while (lagi === true) {
+//     var nama = prompt("Masukan Nama anda : ");
+//     alert("hallo "  nama);
+
+//     lagi = confirm("coba lagi ?");
+// }
+
+// alert("Terimakasih")
+
+// =======================================================================
+
+
