60b5b1a81f3302f5c63eaeb5a6a55f054b06d417
sonihidayatulloh797@gmail.com <sonihidayatulloh797@gmail.com>
2020-02-17 08:00:14 +0700

    add new file

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Hello world")
+
+
+// var person = prompt("Please enter your name", "Soni hidayatulloh");
+
+// if( person == null || person == ""){
+//     txt = "User cecnceled the prompt.";
+// } else {
+//     txt = "Hello" + person + "How are you today ?";
+// }
